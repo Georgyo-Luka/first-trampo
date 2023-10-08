9f7a1110e697f59cbe452dfb2ca5a3df512a5d1a
Georgyo-Luka <134330699+Georgyo-Luka@users.noreply.github.com>
2023-10-08 19:25:15 -0300

    final

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 var menuIcone = document.querySelector('.menu-icone');
 var menuEscondido = document.querySelector('.menu-mobile');
 
+
 menuIcone.addEventListener('click', function() {
   var estaAtivo = menuEscondido.classList.contains('ativo');
 
 
+
   if (estaAtivo) {
-    menuEscondido.classList.remove('ativo');
+    menuEscondido.classList.remove('ativo')
+  ;
+  
   } else {
     menuEscondido.classList.add('ativo');
   }
-});
+
+  }
+);
+
 
 
